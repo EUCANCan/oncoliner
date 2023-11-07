729cb46460dfe603703411f8a640e0a8095c3432
Rapsssito <contact@rodrigomartin.dev>
2023-11-07 15:06:13 +0100

    chore: Remove ASSESMENT_COMMAND from PipelineDesigner example

--- tools/pipeline_designer/example/example.sh
+++ tools/pipeline_designer/example/example.sh
@@ -1,4 +1,3 @@
-export ASSESMENT_COMMAND='python3 ../../../modules/oncoliner_assesment/src/assesment_bulk.py'
 python3 ../src/main.py -c ./example_config.tsv -vc ./input/test -o ./output \
     -p 4 \
     --max-combinations 5 \
